aec8d8c1cbf55fbd3b8cd55a19d96029a452a44e
Thorsten Seyschab <business@todde.tv>
2024-06-28 15:40:26 +0200

    feat: new eslint configurations

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,42 @@
 import antfu from '@antfu/eslint-config'
+import perfectionist from 'eslint-plugin-perfectionist'
 
 export default antfu({
+  formatters: {
+    css: 'prettier',
+    html: 'prettier',
+    markdown: 'prettier',
+  },
+
+  ignores: [
+  ],
+
+  jsonc: true,
+
+  plugins: {
+    perfectionist,
+  },
+
+  rules: {
+    'perfectionist/sort-objects': [
+      'error',
+      {
+        order: 'asc',
+        type: 'natural',
+      },
+    ],
+  },
+
+  stylistic: {
+    indent: 2,
+    quotes: 'single',
+  },
+
+  typescript: {
+    tsconfigPath: 'tsconfig.json',
+  },
+
   vue: true,
+
+  yaml: true,
 })
