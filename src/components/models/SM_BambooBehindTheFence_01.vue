838be904384f061496e0a1d73ecab9a234775a61
Thorsten Seyschab <business@todde.tv>
2024-06-29 01:12:14 +0200

    feat: add Vue components for the new models

---
+++ src/components/models/SM_BambooBehindTheFence_01.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useGLTF } from '@tresjs/cientos'
+
+const { scene } = await useGLTF('/models/japaneseSmallAssets/SM_BambooBehindTheFence_01/SM_BambooBehindTheFence_01.gltf', {
+  draco: true,
+})
+</script>
+
+<template>
+  <primitive :object="scene" />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/models/SM_Bridge_01.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useGLTF } from '@tresjs/cientos'
+
+const { scene } = await useGLTF('/models/japaneseSmallAssets/SM_Bridge_01/SM_Bridge_01.gltf', {
+  draco: true,
+})
+</script>
+
+<template>
+  <primitive :object="scene" />
+</template>
+
+<style scoped>
+</style>
