82a2e05b5ea4ee7ee3da18589d64d9f65b581015
Thorsten Seyschab <business@todde.tv>
2024-07-03 17:57:32 +0200

    feat: unwrap mesh objects so that they are no longer in sub-groups

--- src/components/models/BambooBehindFence.vue
+++ src/components/models/BambooBehindFence.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import { useGLTF } from '@tresjs/cientos'
 
-const { scene } = await useGLTF('/models/japaneseSmallAssets/BambooBehindFence/BambooBehindFence.gltf', {
+const { nodes } = await useGLTF('/models/japaneseSmallAssets/BambooBehindFence/BambooBehindFence.gltf', {
   draco: true,
 })
 </script>
 
 <template>
-  <primitive :object="scene" />
+  <primitive :object="nodes.BambooBehindFence" />
 </template>
 
 <style scoped>
--- src/components/models/Bridge.vue
+++ src/components/models/Bridge.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import { useGLTF } from '@tresjs/cientos'
 
-const { scene } = await useGLTF('/models/japaneseSmallAssets/Bridge/Bridge.gltf', {
+const { nodes } = await useGLTF('/models/japaneseSmallAssets/Bridge/Bridge.gltf', {
   draco: true,
 })
 </script>
 
 <template>
-  <primitive :object="scene" />
+  <primitive :object="nodes.Bridge" />
 </template>
 
 <style scoped>
