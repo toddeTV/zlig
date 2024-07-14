f240fe2d64f27cc5f4f2003437f58ebd002c19bf
Thorsten Seyschab <business@todde.tv>
2024-07-14 23:54:05 +0200

    feat: add test scene

---
+++ src/components/models/TestScene.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useGLTF } from '@tresjs/cientos'
+
+const { nodes, scene } = await useGLTF('/models/TestScene.gltf', {
+  draco: true,
+})
+</script>
+
+<template>
+  <primitive :object="scene" />
+</template>
+
+<style scoped>
+</style>
