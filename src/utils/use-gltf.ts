d1242740539a5d3b196bd6cf9883ffa99b3be5ed
Andreas Fehn <fehnomenal@fehn.systems>
2024-07-15 21:36:32 +0200

    bundle the draco decoder files instead of retrieving them from google

--- src/utils/use-gltf.ts
+++ src/utils/use-gltf.ts
@@ -2,10 +2,35 @@ import { type TresObject3D, trasverseObjects, useLogger } from '@tresjs/core'
 import type { AnimationClip, Material, Scene } from 'three'
 import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js'
 import { type GLTF, GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+import decoderUrl from 'three/examples/jsm/libs/draco/draco_decoder.js?url'
+import wasmUrl from 'three/examples/jsm/libs/draco/draco_decoder.wasm?url'
+import wasmWrapperUrl from 'three/examples/jsm/libs/draco/draco_wasm_wrapper.js?url'
 
-const dracoLoader = new DRACOLoader()
-  .setDecoderPath(`https://www.gstatic.com/draco/versioned/decoders/${import.meta.env.VITE_DRACO_VERSION}/`)
-  .preload()
+// The DRACOLoader uses extra files for decoding the model files. These files are loaded at runtime based on some
+// settings and browser capabilities. For this, the original loader accepts a base URL and appends the file names to it.
+// While this works it is rather cumbersome to include the files that are supplied by the three.js project without
+// manual build steps.
+// To ease the process we take advantage of vite/rollup to allow bundling files and return the url to the asset. In the
+// following subclass we override the known file names to paths that resolve to the assets after the build step.
+
+class BundledDRACOLoader extends DRACOLoader {
+  _loadLibrary(url: string, responseType: string) {
+    if (url === 'draco_decoder.js') {
+      url = decoderUrl
+    }
+    else if (url === 'draco_wasm_wrapper.js') {
+      url = wasmWrapperUrl
+    }
+    else if (url === 'draco_decoder.wasm') {
+      url = wasmUrl
+    }
+
+    // @ts-expect-error This does not exist in the typings...
+    return super._loadLibrary(url, responseType)
+  }
+}
+
+const dracoLoader = new BundledDRACOLoader().preload()
 
 const gltfLoader = new GLTFLoader().setDRACOLoader(dracoLoader)
 
