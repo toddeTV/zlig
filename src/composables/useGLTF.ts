9205e23170639d9d6376e04c3c3b4fad24bb7219
Thorsten Seyschab <business@todde.tv>
2024-07-23 14:59:52 +0200

    refactor: code, doc & adding error logging when no supported draco URL

--- src/composables/useGLTF.ts
+++ src/composables/useGLTF.ts
@@ -6,15 +6,19 @@ import decoderUrl from 'three/examples/jsm/libs/draco/draco_decoder.js?url'
 import wasmUrl from 'three/examples/jsm/libs/draco/draco_decoder.wasm?url'
 import wasmWrapperUrl from 'three/examples/jsm/libs/draco/draco_wasm_wrapper.js?url'
 
-// The DRACOLoader uses extra files for decoding the model files. These files are loaded at runtime based on some
-// settings and browser capabilities. For this, the original loader accepts a base URL and appends the file names to it.
-// While this works it is rather cumbersome to include the files that are supplied by the three.js project without
-// manual build steps.
-// To ease the process we take advantage of vite/rollup to allow bundling files and return the url to the asset. In the
-// following subclass we override the known file names to paths that resolve to the assets after the build step.
+/*
+The DRACOLoader uses extra files for decoding the model files. These files are loaded at runtime based on some
+settings and browser capabilities. For this, the original loader accepts a base URL and appends the file names to it.
+While this works it is rather cumbersome to include the files that are supplied by the three.js project without
+manual build steps.
+To ease the process we take advantage of vite/rollup to allow bundling files and return the url to the asset. In the
+following subclass we override the known file names to paths that resolve to the assets after the build step.
+*/
 
 class BundledDRACOLoader extends DRACOLoader {
   _loadLibrary(url: string, responseType: string) {
+    const { logError } = useLogger()
+
     if (url === 'draco_decoder.js') {
       url = decoderUrl
     }
@@ -24,8 +28,11 @@ class BundledDRACOLoader extends DRACOLoader {
     else if (url === 'draco_decoder.wasm') {
       url = wasmUrl
     }
+    else {
+      logError(`[useGLTF] Unknown DRACO file: ${url}`)
+    }
 
-    // @ts-expect-error This does not exist in the typings...
+    // @ts-expect-error This does not exist in the typings //TODO fix later
     return super._loadLibrary(url, responseType)
   }
 }
@@ -41,6 +48,9 @@ export interface GLTFResult extends GLTF {
   scene: Scene & GLTF['scene']
 }
 
+/**
+ * Loads a GLTF model from the specified path.
+ */
 export default async function (
   path: string,
 ): Promise<GLTFResult> {
@@ -53,6 +63,6 @@ export default async function (
       }
 
       resolve(model as GLTFResult)
-    }, undefined, event => reject(logError('[useGLTF] - Failed to load resource', event)))
+    }, undefined, event => reject(logError('[useGLTF] Failed to load resource', event)))
   })
 }
