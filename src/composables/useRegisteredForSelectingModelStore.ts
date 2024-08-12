fde38540af393a3590a64dc7acb10f85d5770421
Thorsten Seyschab <business@todde.tv>
2024-08-12 19:49:44 +0200

    doc: add documentation to all store helper functions

--- src/composables/useRegisteredForSelectingModelStore.ts
+++ src/composables/useRegisteredForSelectingModelStore.ts
@@ -3,17 +3,34 @@ import type { Object3D } from 'three'
 import { reactive } from 'vue'
 
 export default defineStore('registeredForSelectingModelStore', () => {
-  // store only the `id` of the `Object3D` object for performance reasons
+  // Store only the `id` of the `Object3D` object for performance reasons
   const registered = reactive(new Set<number>())
 
+  /**
+   * Retrieves all registered Object3D IDs.
+   *
+   * @returns {number[]} - An array of registered Object3D IDs.
+   */
   function getRegisteredAll(): number[] {
     return Array.from(registered)
   }
 
+  /**
+   * Checks if the given Object3D instance is registered.
+   *
+   * @param {Object3D} obj - The Object3D instance to check.
+   * @returns {boolean} - Returns true if the Object3D instance is registered, otherwise false.
+   */
   function isRegistered(obj: Object3D): boolean {
     return registered.has(getObject3DId(obj))
   }
 
+  /**
+   * Registers the given Object3D instance.
+   *
+   * @param {Object3D} obj - The Object3D instance to register.
+   * @returns {boolean} - Returns true if the Object3D instance was successfully registered, otherwise false.
+   */
   function register(obj: Object3D): boolean {
     if (isRegistered(obj)) {
       return false
@@ -22,6 +39,12 @@ export default defineStore('registeredForSelectingModelStore', () => {
     return true
   }
 
+  /**
+   * Unregisters the given Object3D instance.
+   *
+   * @param {Object3D} obj - The Object3D instance to unregister.
+   * @returns {boolean} - Returns true if the Object3D instance was successfully unregistered, otherwise false.
+   */
   function unregister(obj: Object3D): boolean {
     if (!isRegistered(obj)) {
       return false
@@ -30,10 +53,19 @@ export default defineStore('registeredForSelectingModelStore', () => {
     return true
   }
 
+  /**
+   * Unregisters all Object3D instances.
+   */
   function unregisterAll() {
     registered.clear()
   }
 
+  /**
+   * Retrieves the ID of the given Object3D instance.
+   *
+   * @param {Object3D} obj - The Object3D instance to get the ID from.
+   * @returns {number} - The ID of the Object3D instance.
+   */
   function getObject3DId(obj: Object3D): number {
     return obj.id
   }
