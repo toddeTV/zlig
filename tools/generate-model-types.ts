2c2af37c9da3ba661d2d2fb0600fce366a839a39
Thorsten Seyschab <business@todde.tv>
2024-08-17 15:37:49 +0200

    feat: improved gltf model types generator with stricter and correct typs

--- tools/generate-model-types.ts
+++ tools/generate-model-types.ts
@@ -7,12 +7,6 @@ const modelFileExtensions = ['.gltf']
 const modelScanDirectory = join('src', 'assets')
 const typeOutputDirectory = resolve('node_modules', '.tmp', 'model-types')
 
-const topLevelKeyTypes: Partial<Record<string, string>> = {
-  meshes: `import('three').Mesh`,
-  nodes: `import('three').Object3D`,
-  scenes: `import('three').Scene`,
-}
-
 // Also allow running this as a script besides allowing to import this module.
 if (fileURLToPath(import.meta.url) === argv[1]) {
   await generateAllModelTypes()
@@ -75,6 +69,9 @@ async function getAllModels() {
   return models
 }
 
+/**
+ * The result here must be the same as in `useGLTF.ts`.
+ */
 async function getAmbientModuleCode(model: Model) {
   const content = [
     ...(await getGeneratedTypeLines(model)),
@@ -86,32 +83,151 @@ async function getAmbientModuleCode(model: Model) {
   return content
 }
 
-async function getGeneratedTypeLines(model: Model) {
-  const modelJson = JSON.parse(await readFile(model.filePath, { encoding: 'utf-8' }))
-  const topLevelKeysWithNamedArray = Object.entries(modelJson)
-    .filter((entry): entry is [string, { name: string }[]] => {
-      if (!Array.isArray(entry[1])) {
-        return false
-      }
+function escapeKeyString(key: string) {
+  return key.replaceAll('"', '\\"').replaceAll('.', '')
+}
 
-      return entry[1].every(item => typeof item === 'object' && 'name' in item && typeof item.name === 'string')
-    })
+async function getGeneratedTypeLines(model: Model) {
+  const modelJson = JSON.parse(await readFile(model.filePath, { encoding: 'utf-8' })) as {
+    animations?: {
+      name: string
+    }[]
+    asset: {
+      copyright?: string | undefined
+      generator?: string | undefined
+      version?: string | undefined
+      minVersion?: string | undefined
+      extensions?: any
+      extras?: any
+    }
+    nodes?: {
+      name: string
+      mesh?: number
+      camera?: number
+    }[]
+    scenes?: {
+      name: string
+      nodes: number[]
+    }[]
+    meshes?: {
+      name: string
+      primitives: {
+        material: number
+      }[]
+    }[]
+    materials?: any[]
+    cameras?: any[]
+  }
 
   const generatedType: string[] = []
 
   generatedType.push(`export interface ${model.name} {`)
 
-  for (const [key, items] of topLevelKeysWithNamedArray) {
-    const type = topLevelKeyTypes[key] ?? 'any'
-
-    generatedType.push(`  ${key}: {`)
-
-    for (const item of items) {
-      generatedType.push(`    '${item.name}': ${type}`)
-    }
-
-    generatedType.push(`  }`)
+  // `animations`
+  generatedType.push(`  animations: {`)
+  if (modelJson.animations !== undefined) {
+    generatedType.push(
+      ...(modelJson.animations).map(
+        animation => `    "${escapeKeyString(animation.name)}": import('three').AnimationClip`,
+      ),
+    )
   }
+  generatedType.push(`  }`)
+
+  // `asset`
+  generatedType.push( // hardcoded, could check what keys are really present //TODO
+    `  asset: {`,
+    `    copyright?: string | undefined`,
+    `    generator?: string | undefined`,
+    `    version?: string | undefined`,
+    `    minVersion?: string | undefined`,
+    `    extensions?: any`,
+    `    extras?: any`,
+    `  }`,
+  )
+
+  // `parser`
+  generatedType.push(`  parser: import('three').GLTFParser`)
+
+  // `scenes`
+  generatedType.push(`  scenes: {`)
+  if (modelJson.scenes !== undefined) {
+    for (const scene of modelJson.scenes) {
+      generatedType.push(
+        `    "${escapeKeyString(scene.name)}": import('three').Group & {`,
+        `      traversed: {`,
+        `        Object: {`,
+      )
+      if (modelJson.nodes !== undefined && modelJson.meshes !== undefined) {
+        generatedType.push(
+          ...modelJson.nodes.filter(
+            node => scene.nodes.includes(modelJson.nodes!.indexOf(node)) && node.mesh !== undefined,
+          ).map((meshAndGroup) => {
+            const meshAndGroupMeshes = modelJson.meshes![meshAndGroup.mesh!]
+            const importString = meshAndGroupMeshes.primitives.length <= 1
+              ? `import('three').Mesh`
+              : `import('three').Group`
+            return `          "${escapeKeyString(meshAndGroup.name)}": ${importString}`
+          }),
+        )
+      }
+      generatedType.push(
+        `        }`,
+        `        Material: {`,
+      )
+      if (modelJson.nodes !== undefined && modelJson.meshes !== undefined) {
+        const meshAndGroups = modelJson.nodes.filter(
+          node => scene.nodes.includes(modelJson.nodes!.indexOf(node)) && node.mesh !== undefined,
+        )
+        const materials = new Set<number>()
+        for (const meshAndGroup of meshAndGroups) {
+          const meshAndGroupMeshes = modelJson.meshes![meshAndGroup.mesh!]
+          const flattenedMaterials = meshAndGroupMeshes.primitives.map(primitive => primitive.material)
+          flattenedMaterials.forEach(material => materials.add(material))
+        }
+        generatedType.push(
+          ...modelJson.materials!.filter((material, index) => materials.has(index))
+            .map(mat => `          "${escapeKeyString(mat.name)}": import('three').Material`),
+        )
+      }
+      generatedType.push(
+        `        }`,
+        // TODO add the correct typd lights, currently this is too much work for the scope of the project, and also
+        // the fallback at the end will catch this -> so it works, it only is not typed
+        `        Light: Record<String, import('three').Light>`,
+        `        Camera: {`,
+      )
+      if (modelJson.nodes !== undefined && modelJson.cameras !== undefined) {
+        generatedType.push(
+          ...modelJson.nodes.filter(
+            node => scene.nodes.includes(modelJson.nodes!.indexOf(node)) && node.camera !== undefined,
+          ).map(meshAndGroup => `          "${escapeKeyString(meshAndGroup.name)}": import('three').Camera`),
+        )
+      }
+      generatedType.push(
+        `        }`,
+        `        [key: string]: Record<string, any>`,
+        `      }`,
+        `    }`,
+      )
+    }
+  }
+  generatedType.push(`  }`)
+
+  // `userData`
+  generatedType.push(`  userData: Record<string, any>`)
+
+  // for (const [key, type] of Object.entries({
+  //   materials: 'any',
+  // })) {
+  //   if (key in modelJson) {
+  //     generatedType.push(
+  //       `  ${key}: {`,
+  //       ...(modelJson[key] as NamedItems).map(item => `    "${escapeKeyString(item.name)}": ${type}`),
+  //       `  }`,
+  //     )
+  //   }
+  // }
 
   generatedType.push(`}`)
 
