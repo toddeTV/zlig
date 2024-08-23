ca71bbbcffb75f4d232d1e271023fb0262dcf6e5
Andreas Fehn <fehnomenal@fehn.systems>
2024-08-23 09:30:10 +0200

    add index for all building classes

---
+++ src/game-logic/building-classes/index.ts
@@ -0,0 +1,4 @@
+import { well } from '@/game-logic/building-classes/well.js'
+import { windmill } from '@/game-logic/building-classes/windmill.js'
+
+export const buildingClasses = { well, windmill }
