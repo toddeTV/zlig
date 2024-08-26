c906249acab1eaa2edf12f01576fa88ab0ffd865
Thorsten Seyschab <business@todde.tv>
2024-08-26 13:24:25 +0200

    feat: add Island model (#44)

---
+++ src/components/models/Island.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import modelLoader from '@/assets/models/Island/Island.gltf'
+import useRegisteredForSelectingModelStore from '@/composables/useRegisteredForSelectingModelStore'
+
+const registeredForSelectingModelStore = useRegisteredForSelectingModelStore()
+
+const { scenes } = await modelLoader
+
+const scene = scenes.Scene
+
+Object.entries(scene.Object).forEach(([key, value]) => {
+  if (key.startsWith('buildArea')) {
+    registeredForSelectingModelStore.register(value)
+  }
+})
+</script>
+
+<template>
+  <primitive :object="scene.Scene" />
+</template>
+
+<style scoped>
+</style>
