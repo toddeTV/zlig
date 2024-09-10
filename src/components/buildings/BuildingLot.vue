6fb23e5e837c36888a69a2fc78a39a8e258ed511
Andreas Fehn <fehnomenal@fehn.systems>
2024-09-11 00:46:58 +0200

    display billboard popup for selected building lot

--- src/components/buildings/BuildingLot.vue
+++ src/components/buildings/BuildingLot.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { BoxGeometry, Mesh, MeshBasicMaterial, Vector3 } from 'three'
+import { Box, Html } from '@tresjs/cientos'
+import { Vector3 } from 'three'
 import { computed } from 'vue'
 import modelLoader from '@/assets/models/Island/Island.gltf'
 import useSelectedBuildingLot from '@/composables/useSelectedBuildingLot.js'
@@ -10,13 +11,11 @@ const props = defineProps<{
   position: Vector3
 }>()
 
-const geometry = new BoxGeometry(1, 1, 1)
-const material = new MeshBasicMaterial({ color: 0x00FF00 })
-const cube = new Mesh(geometry, material)
-
 const { scenes: { Scene } } = await modelLoader
 
 const selectedBuildingLot = useSelectedBuildingLot()
+
+const isSelected = computed(() => selectedBuildingLot.id === props.id)
 
 function onClick(e: TresJsClickEvent) {
   e.stopPropagation()
@@ -24,12 +23,24 @@ function onClick(e: TresJsClickEvent) {
   selectedBuildingLot.id = props.id
 }
 
-const position = computed(() => selectedBuildingLot.id === props.id ? new Vector3(0, 1, 0).add(props.position) : props.position)
+const offset = computed(() => new Vector3(0, isSelected.value ? 1 : 0.5, 0))
 </script>
 
 <template>
   <TresGroup @click="onClick">
-    <primitive :object="cube" :position="position" />
+    <Box :args="[1, 1, 1]" :position="new Vector3(0, 0, 0).add(props.position).add(offset)">
+      <TresMeshBasicMaterial color="lightgreen" />
+    </Box>
+
+    <Html
+      v-if="isSelected"
+      center
+      :position="new Vector3(0, 3, 0).add(props.position).add(offset)"
+    >
+      <h1 class="bg-white dark:bg-dark p-1 rounded w-[100px]" @click="console.log">
+        I'm a Box 📦
+      </h1>
+    </Html>
 
     <primitive :object="Scene.Object[props.id as keyof typeof Scene.Object]" />
   </TresGroup>
