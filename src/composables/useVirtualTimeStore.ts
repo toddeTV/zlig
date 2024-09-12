555f1c9ebddfb2c9b8e40100ebcf6fca3155f17e
Thorsten Seyschab <business@todde.tv>
2024-09-12 20:12:57 +0200

    chore: set virtual time more fine granular

--- src/composables/useVirtualTimeStore.ts
+++ src/composables/useVirtualTimeStore.ts
@@ -3,11 +3,12 @@ import { onMounted, readonly, ref } from 'vue'
 
 export default defineStore('virtualTimeStore', () => {
   // the storing of the virtual time
-  const currentVirtualTime = ref(new Date(1, 1, 1970, 8, 0, 0, 0)) // the store
+  const currentVirtualTime = ref(new Date(1, 1, 1970, 0, 0, 0, 0)) // the store
 
+  // TODO must be calculated with the animated frames better than an external interval
   // the interval for updating the virtual time
-  const virtualTimeMultiplier = 1000 // `1` means real time, `2` means 2x speed, `0.5` means half speed, etc.
-  const intervalSpeed = 500 // in milliseconds, how often to update the virtual time
+  const virtualTimeMultiplier = 5000 // `1` means real time, `2` means 2x speed, `0.5` means half speed, etc.
+  const intervalSpeed = 100 // in milliseconds, how often to update the virtual time
   onMounted(() => {
     setInterval(() => {
       currentVirtualTime.value = new Date(currentVirtualTime.value.getTime() + virtualTimeMultiplier * intervalSpeed)
