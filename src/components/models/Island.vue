2f20597ba008a2b6a5a89271b4a63ec83886d950
Thorsten Seyschab <business@todde.tv>
2024-11-05 10:58:09 +0100

    fix: Island 3D model usage

--- src/components/models/Island.vue
+++ src/components/models/Island.vue
@@ -7,35 +7,12 @@ const { scene } = useTresContext()
 const { addShadowAndAddToGroup } = useThreeHelper()
 const { scenes } = await modelLoader
 
-const sceneGroup = scene.value.getObjectByName('sceneGroup') ?? scene.value
-const islandScene = scenes.Island
+addShadowAndAddToGroup(scene.value, scenes.Island.Scene)
 
-addShadowAndAddToGroup(sceneGroup, islandScene.Object.water)
-addShadowAndAddToGroup(sceneGroup, islandScene.Object.island_lvl4_grass)
-addShadowAndAddToGroup(sceneGroup, islandScene.Object.island_lvl2_beach)
-addShadowAndAddToGroup(sceneGroup, islandScene.Object.island_lvl3_grass)
-addShadowAndAddToGroup(sceneGroup, islandScene.Object.island_lvl5_grass)
-addShadowAndAddToGroup(sceneGroup, islandScene.Object.island_lvl1_underBeach)
-addShadowAndAddToGroup(sceneGroup, islandScene.Object.island_lvl0_seabed)
-addShadowAndAddToGroup(sceneGroup, islandScene.Object.water)
-// addShadowAndAddToGroup(sceneGroup, islandScene.Object.buildArea00)
-// addShadowAndAddToGroup(sceneGroup, islandScene.Object.buildArea03)
-// addShadowAndAddToGroup(sceneGroup, islandScene.Object.buildArea02)
-// addShadowAndAddToGroup(sceneGroup, islandScene.Object.buildArea01)
-// addShadowAndAddToGroup(sceneGroup, islandScene.Object.buildArea07)
-// addShadowAndAddToGroup(sceneGroup, islandScene.Object.buildArea04)
-// addShadowAndAddToGroup(sceneGroup, islandScene.Object.buildArea05)
-// addShadowAndAddToGroup(sceneGroup, islandScene.Object.buildArea06)
-// addShadowAndAddToGroup(sceneGroup, islandScene.Object.buildArea08)
-// addShadowAndAddToGroup(sceneGroup, islandScene.Object.buildArea09)
-// addShadowAndAddToGroup(sceneGroup, islandScene.Object.buildArea10)
-// addShadowAndAddToGroup(sceneGroup, islandScene.Object.buildArea11)
-addShadowAndAddToGroup(sceneGroup, islandScene.Object.waterfall_lvl1)
-addShadowAndAddToGroup(sceneGroup, islandScene.Object.waterfall_lvl0)
-addShadowAndAddToGroup(sceneGroup, islandScene.Object.waterfall_lvl1_to_lvl0)
-addShadowAndAddToGroup(sceneGroup, islandScene.Object.mountain_lvl1)
-addShadowAndAddToGroup(sceneGroup, islandScene.Object.mountain_lvl0)
-addShadowAndAddToGroup(sceneGroup, islandScene.Object.mountain_lvl3)
+// const sceneGroup = scene.value.getObjectByName('sceneGroup') ?? scene.value
+// const islandScene = scenes.Island
+// addShadowAndAddToGroup(sceneGroup, islandScene.Object.island_lvl4_grass)
+// addShadowAndAddToGroup(sceneGroup, islandScene.Object.island_lvl2_beach)
 </script>
 
 <!-- eslint-disable-next-line vue/valid-template-root -->
