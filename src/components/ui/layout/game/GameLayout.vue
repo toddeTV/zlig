94020acc5953b54a0a64fd38a554c3792ae0b87f
Andreas Fehn <fehnomenal@fehn.systems>
2024-11-07 21:18:09 +0100

    no need for a checkbox, just use a js variable

--- src/components/ui/layout/game/GameLayout.vue
+++ src/components/ui/layout/game/GameLayout.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import SideBar from './SideBar.vue'
 import TopBar from './TopBar.vue'
 import WorldWindow from './WorldWindow.vue'
 
-const sidebarToggleId = 'sidebar-toggle'
+const sidebarOpen = ref(false)
+function toggleSidebar() {
+  sidebarOpen.value = !sidebarOpen.value
+}
 </script>
 
 <template>
@@ -11,40 +15,39 @@ const sidebarToggleId = 'sidebar-toggle'
     <TopBar />
 
     <div class="relative flex flex-grow">
-      <input :id="sidebarToggleId" class="hidden peer" type="checkbox">
-
-      <label
+      <span
         class="
           md:hidden
-          absolute left-0 top-[7.5%]
-          border shadow rounded-r-full pl-1 py-2 pr-3 flex items-center text-xl z-10 bg-white/75
+          absolute left-0 top-[7.5%] transition-[left]
+          shadow rounded-r-full pl-1 py-2 pr-3 flex items-center text-xl z-20 bg-white/75
         "
-        :for="sidebarToggleId"
+        :class="{ 'left-[90%]': sidebarOpen }"
         title="Toggle menu"
+        @click="toggleSidebar"
       >
-        <span class="icon-[ph--list]" />
-      </label>
+        <span :class="{ 'icon-[ph--list]': !sidebarOpen, 'icon-[ph--x]': sidebarOpen }" />
+      </span>
 
       <div
         class="
           absolute md:relative md:left-0 right-0 top-0 bottom-0
-          -left-[100%] peer-checked:left-0
-          transition-[left]
+          -left-[100%] transition-[left]
           max-w-[90%] md:max-w-none md:w-[25%] lg:w-[22.5%] xl:w-[20%]
-          overflow-hidden z-20 shadow
+          overflow-hidden z-30 shadow
         "
+        :class="{ 'left-0': sidebarOpen }"
       >
-        <SideBar :sidebar-toggle-id />
+        <SideBar />
       </div>
 
-      <label
+      <span
         class="
           absolute left-0 right-0 top-0 bottom-0 md:hidden
-          opacity-0 peer-checked:opacity-50
-          pointer-events-none peer-checked:pointer-events-auto
+          opacity-0 pointer-events-none
           transition-opacity bg-black z-10
         "
-        :for="sidebarToggleId"
+        :class="{ 'opacity-50': sidebarOpen, 'pointer-events-auto': sidebarOpen }"
+        @click="toggleSidebar"
       />
 
       <div class=" flex-grow">
--- src/components/ui/layout/game/SideBar.vue
+++ src/components/ui/layout/game/SideBar.vue
@@ -1,18 +1,8 @@
 <script setup lang="ts">
-const { sidebarToggleId } = defineProps({
-  sidebarToggleId: {
-    required: true,
-    type: String,
-  },
-})
 </script>
 
 <template>
   <div class="bg-blue-100 h-full p-3">
-    <label class="md:hidden flex items-center justify-end text-xl" :for="sidebarToggleId" title="Close menu">
-      <span class="icon-[ph--x]" />
-    </label>
-
     <code>sidebar</code>
   </div>
 </template>
