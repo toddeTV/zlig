9c0bddd31f4afa9f239678888b3226c58e843058
Andreas Fehn <fehnomenal@fehn.systems>
2024-11-09 22:15:00 +0100

    use new layout

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,45 +1,58 @@
 <script setup lang="ts">
-import GameEngine from '@/components/GameEngine.vue'
-import useVirtualTimeStore from '@/composables/useVirtualTimeStore'
-import { TresCanvas } from '@tresjs/core'
-import { storeToRefs } from 'pinia'
-import { NoToneMapping, SRGBColorSpace, VSMShadowMap } from 'three'
-import { ref, watch } from 'vue'
-import type { TresCanvasProps } from '@tresjs/core/dist/src/components/TresCanvas.vue.js'
+import SideBar from '@/components/ui/layout/game/sidebar/SideBar.vue'
+import TopBar from '@/components/ui/layout/game/TopBar.vue'
+import WorldWindow from '@/components/ui/layout/game/WorldWindow.vue'
+import { ref } from 'vue'
 
-const { getColorByTime, rgbToHex, skyTimeColorTransitionPreset } = useVirtualTimeStore()
-const { currentVirtualTime } = storeToRefs(useVirtualTimeStore())
-
-const gl = ref<TresCanvasProps>({
-  alpha: false,
-  clearColor: '#82DBC5',
-  disableRender: true, // Disable render on requestAnimationFrame, useful for PostProcessing // TODO use or not?
-  outputColorSpace: SRGBColorSpace,
-  renderMode: 'always',
-  // `VSMShadowMap` better shadows, but more performance heavy; `BasicShadowMap` is faster but has less quality
-  shadowMapType: VSMShadowMap, // BasicShadowMap | PCFShadowMap | PCFSoftShadowMap | VSMShadowMap
-  shadows: true,
-  toneMapping: NoToneMapping,
-  useLegacyLights: false,
-})
-
-// watch the current virtual time
-watch(() => currentVirtualTime.value, (newValue, _oldValue) => {
-  // set the sky color
-  gl.value.clearColor = rgbToHex(getColorByTime(newValue, skyTimeColorTransitionPreset))
-})
+const sidebarOpen = ref(false)
+function toggleSidebar() {
+  sidebarOpen.value = !sidebarOpen.value
+}
 </script>
 
-<!-- @pointer-missed="() => firePointerMissedEvent()" -->
 <template>
-  <div class="h-full overflow-hidden relative">
-    <TresCanvas
-      v-bind="gl"
-    >
-      <GameEngine />
-    </TresCanvas>
+  <div class="flex flex-col flex-grow">
+    <TopBar />
+
+    <div class="relative flex flex-grow">
+      <span
+        class="
+          md:hidden
+          absolute left-0 top-[7.5%] transition-[left]
+          shadow rounded-r-full pl-1 py-2 pr-3 flex items-center text-xl z-20 bg-white/75
+        "
+        :class="{ 'left-[90%]': sidebarOpen }"
+        title="Toggle menu"
+        @click="toggleSidebar"
+      >
+        <span :class="{ 'icon-[ph--list]': !sidebarOpen, 'icon-[ph--x]': sidebarOpen }" />
+      </span>
+
+      <div
+        class="
+          absolute md:relative md:left-0 right-0 top-0 bottom-0
+          -left-[100%] transition-[left]
+          max-w-[90%] md:max-w-none md:w-[25%] lg:w-[22.5%] xl:w-[20%]
+          overflow-hidden z-30 shadow
+        "
+        :class="{ 'left-0': sidebarOpen }"
+      >
+        <SideBar />
+      </div>
+
+      <span
+        class="
+          absolute left-0 right-0 top-0 bottom-0 md:hidden
+          opacity-0 pointer-events-none
+          transition-opacity bg-black z-10
+        "
+        :class="{ 'opacity-50': sidebarOpen, 'pointer-events-auto': sidebarOpen }"
+        @click="toggleSidebar"
+      />
+
+      <div class=" flex-grow">
+        <WorldWindow />
+      </div>
+    </div>
   </div>
 </template>
-
-<style scoped>
-</style>
