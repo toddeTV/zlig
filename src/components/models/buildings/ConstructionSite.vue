9009e488430d1585d4c3ffd84590988ca00f1af3
Thorsten Seyschab <business@todde.tv>
2024-11-09 01:21:08 +0100

    feat: new construction site implementation with empty object idea

--- src/components/models/buildings/ConstructionSite.vue
+++ src/components/models/buildings/ConstructionSite.vue
@@ -1,13 +1,40 @@
 <script setup lang="ts">
 import modelLoader from '@/assets/models/BuildArea/BuildArea.gltf'
+import { type TresInstance, type TresPrimitive, useTresContext } from '@tresjs/core'
+import { Color, type Euler, Group, type Intersection, Mesh, type Object3D, type Vector3 } from 'three'
+import { onMounted, ref, shallowRef } from 'vue'
+import type { BuildingAreaId } from '@/game-logic/types'
+
+const props = defineProps<{
+  buildingAreaId: BuildingAreaId
+  position: Vector3
+  rotation: Euler
+}>()
+
+const obj = shallowRef<TresPrimitive>()
+const modelObject = ref(new Group())
 
 const { scenes } = await modelLoader
 
-// TODO: Create and use a real model here.
+onMounted(() => {
+  if (!obj.value) {
+    return
+  }
+
+  // const model = scenes.BuildArea.Object.zligbuildAreabase001.clone()
+  const model = scenes.BuildArea.Scene.clone()
+  model.position.copy(props.position)
+  model.rotation.copy(props.rotation)
+  modelObject.value = model
+})
 </script>
 
 <template>
   <primitive
-    :object="scenes.BuildArea.Object.zligbuildAreabase001.clone()"
+    ref="obj"
+    :object="modelObject"
   />
 </template>
+
+<style scoped>
+</style>
