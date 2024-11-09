e680b5a637a59b79234346b4593873f298996a2a
Thorsten Seyschab <business@todde.tv>
2024-11-09 11:55:09 +0100

    fix: problems from merging

--- src/components/GameEngine.vue
+++ src/components/GameEngine.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 import useBuildingAreas from '@/composables/useBuildingAreas.js'
-import useSelectedBuildingArea from '@/composables/useSelectedBuildingArea.js'
+import useSelectedBuildingArea from '@/composables/useSelectedBuildingAreaStore.js'
+import { getLeafObjects } from '@/utils/threeHelper'
+import { useTresContext } from '@tresjs/core'
+import { EffectComposer, Outline } from '@tresjs/post-processing'
 import { storeToRefs } from 'pinia'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import BuildingArea from './buildings/BuildingArea.vue'
 import CameraAndControls from './CameraAndControls.vue'
 import DistanceFog from './DistanceFog.vue'
@@ -11,6 +14,7 @@ import Island from './models/Island.vue'
 import Ocean from './models/Ocean.vue'
 import VisualHelper from './VisualHelper.vue'
 
+const { scene } = useTresContext()
 const { areas } = storeToRefs(useBuildingAreas())
 const { init } = useBuildingAreas()
 init()
