e0b4ac23b9befa63972a8f9706138a723c1b0825
Thorsten Seyschab <business@todde.tv>
2024-11-11 20:18:14 +0100

    chore: remove test building (#104)

--- src/game-logic/buildings/index.ts
+++ src/game-logic/buildings/index.ts
@@ -1,9 +1,7 @@
 import carnationShop from '@/game-logic/buildings/carnationShop'
-import testBuilding from '@/game-logic/buildings/testBuilding'
 import windmill from '@/game-logic/buildings/windmill'
 
 export const buildingTypes = {
   carnationShop,
-  testBuilding,
   windmill,
 }
