9c0d1ebd2b3d7ae2edd64d4dee0c0c083bd599f3
Thorsten Seyschab <business@todde.tv>
2024-11-11 18:39:02 +0100

    refactor: rename

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
+import GameCanvas from '@/components/ui/layout/game/GameCanvas.vue'
 import SideBar from '@/components/ui/layout/game/sidebar/SideBar.vue'
 import TopBar from '@/components/ui/layout/game/topbar/TopBar.vue'
-import WorldWindow from '@/components/ui/layout/game/WorldWindow.vue'
 import useSelectedBuildingArea from '@/composables/useSelectedBuildingArea.js'
 import { ref } from 'vue'
 
@@ -60,7 +60,7 @@ selectedBuildingArea.$subscribe((_, newId) => {
       />
 
       <div class=" flex-grow">
-        <WorldWindow />
+        <GameCanvas />
       </div>
     </div>
   </div>
