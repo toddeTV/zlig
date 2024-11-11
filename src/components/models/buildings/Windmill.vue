0302bc03e8abcd4c6d140a9892dafa0b0607dbf6
Thorsten Seyschab <business@todde.tv>
2024-11-11 19:56:53 +0100

    feat: animate windmill wind turbine

--- src/components/models/buildings/Windmill.vue
+++ src/components/models/buildings/Windmill.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import modelLoader from '@/assets/models/_Buildings/Windmill/Windmill.gltf'
 import { addShadowAndAddToGroup } from '@/utils/threeHelper'
+import { useLoop } from '@tresjs/core'
 import { shallowRef, toRef, watch } from 'vue'
 import type { BuildingAreaId, BuildingInstance } from '@/game-logic/types.js'
 import type { Euler, Group, Vector3 } from 'three'
@@ -14,6 +15,7 @@ const props = defineProps<{
 
 const buildingInstance = toRef(props, 'buildingInstance')
 
+const { onBeforeRender } = useLoop()
 const { scenes: { Windmill } } = await modelLoader
 
 const groupWrapperRef = shallowRef<Group>()
@@ -32,6 +34,22 @@ watch(groupWrapperRef, (newValue) => {
 watch(buildingInstance, (newValue) => {
   // scale size infinitely linearly
   building.scale.setScalar(1 + 0.1 * newValue.level)
+})
+
+// animate the windmill
+// watch(currentTime, (time, prev) => { // relate to in-game time
+//   const deltaMs = time.getTime() - prev.getTime()
+onBeforeRender(({ elapsed }) => { // relate on renderer
+  if (!groupWrapperRef.value) {
+    return
+  }
+  // TODO do not use string in `getObjectByName` bc string will not throw error on build time
+  const windTurbine = groupWrapperRef.value.getObjectByName('ea3windmillwindTurbine001')
+  if (!windTurbine) {
+    console.error('Windmill: windTurbine not found')
+    return
+  }
+  windTurbine.rotation.x += elapsed * 0.00015
 })
 </script>
 
