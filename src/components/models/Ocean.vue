e602dc27ee5bef47d346d58b8d729b08aab7ee64
Thorsten Seyschab <business@todde.tv>
2024-11-11 12:41:22 +0100

    fix: ocean shader material

--- src/components/models/Ocean.vue
+++ src/components/models/Ocean.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import modelLoader from '@/assets/models/Ocean/Ocean.gltf'
 import useDebugStore from '@/composables/useDebugStore'
-import { addShadow, addToGroup } from '@/utils/threeHelper'
+import { addShadow, addShadowAndAddToGroup, addToGroup } from '@/utils/threeHelper'
 import { getWaterMaterial } from '@/utils/WaterShader'
 import { type TresPrimitive, useLoop, useTresContext } from '@tresjs/core'
 import { storeToRefs } from 'pinia'
-import { Group, Mesh, PlaneGeometry } from 'three'
+import { Group, Mesh, Plane, PlaneGeometry } from 'three'
 import { onMounted, ref, shallowRef, watch } from 'vue'
 import type { Object3D } from 'three'
 
@@ -13,46 +13,47 @@ const { scenes } = await modelLoader
 const { onBeforeRender } = useLoop()
 const { showWaterWireframe } = storeToRefs(useDebugStore())
 
-const obj = shallowRef<TresPrimitive>()
+const groupWrapperRef = shallowRef()
 
 const uniforms = ref({
   time: { value: 0.0 },
 })
 
-const oceanMaterial = getWaterMaterial({
-  waveSpeed: 0.8,
+const waterMaterial = getWaterMaterial({
+  relativeHeightOffset: -0.68,
+  waveAmplitude: 2.0,
+  waveSpeed: 1.0,
   waveTangentialAmplitude: 1.0,
 })
 
-onBeforeRender(({ delta, scene }) => {
-  if (!obj.value || !obj.value.material.userData.shader) {
+onBeforeRender(({ delta }) => {
+  if (groupWrapperRef.value.children.length === 0) {
+    return
+  }
+  if (!groupWrapperRef.value.children[0].material.userData.shader) {
     return
   }
   uniforms.value.time.value += delta
-  obj.value.material.userData.shader.uniforms.time = uniforms.value.time
+  groupWrapperRef.value.children[0].material.userData.shader.uniforms.time = uniforms.value.time
 })
 
 watch(showWaterWireframe, () => {
-  oceanMaterial.wireframe = showWaterWireframe.value
+  waterMaterial.wireframe = showWaterWireframe.value
 }, {
   immediate: true,
 })
 
 onMounted(() => {
-  if (!obj.value) {
-    return
-  }
-  obj.value.material = oceanMaterial
-  obj.value.position.y += 0.18 // move water plane right up to the edge of the beach as the waves are all lower than this
-  addShadow(obj.value, 'receive')
+  const model = scenes.Ocean.Object.ocean001.clone()
+  model.material = waterMaterial
+  addShadowAndAddToGroup(groupWrapperRef.value, model, 'receive')
 })
 </script>
 
 <!-- eslint-disable-next-line vue/valid-template-root -->
 <template>
-  <primitive
-    ref="obj"
-    :object="scenes.Ocean.Object.ocean001.clone()"
+  <TresGroup
+    ref="groupWrapperRef"
   />
 </template>
 
