5dd7a6fb483c99311c5627bb4b4574af84c50e8f
Andreas Fehn <fehnomenal@fehn.systems>
2024-11-12 09:07:36 +0100

    chore: modify state directly instead of patching

--- src/components/buildings/behaviors/ConstructingBehavior.vue
+++ src/components/buildings/behaviors/ConstructingBehavior.vue
@@ -13,31 +13,29 @@ const props = defineProps<{
   state: BuildingStateInConstruction
 }>()
 
-const gameState = useGameState()
+const { buildings } = storeToRefs(useGameState())
 const { currentTime } = storeToRefs(useGameTime())
 
 watch(currentTime, (time, prev) => {
   const durationRemaining = props.state.durationRemaining.minus(Duration.fromDates(prev, time))
 
-  gameState.$patch((state) => {
-    if (durationRemaining.milliseconds.gt(0)) {
-      state.buildings[props.areaId] = {
-        durationRemaining,
-        initialDuration: props.state.initialDuration,
-        level: 0,
-        state: 'in-construction',
-        type: props.buildingType,
-      }
+  if (durationRemaining.milliseconds.gt(0)) {
+    buildings.value[props.areaId] = {
+      durationRemaining,
+      initialDuration: props.state.initialDuration,
+      level: 0,
+      state: 'in-construction',
+      type: props.buildingType,
     }
-    else {
-      state.buildings[props.areaId] = {
-        internalBuffer: new ResourceRecord(),
-        level: 1,
-        state: 'producing',
-        type: props.buildingType,
-      }
+  }
+  else {
+    buildings.value[props.areaId] = {
+      internalBuffer: new ResourceRecord(),
+      level: 1,
+      state: 'producing',
+      type: props.buildingType,
     }
-  })
+  }
 })
 </script>
 
