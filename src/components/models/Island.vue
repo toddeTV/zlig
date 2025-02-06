7b73ef193c115c38d3b4952c55f5ca8a4ecc230d
Thorsten Seyschab <business@todde.tv>
2025-02-06 18:30:42 +0100

    chore: code beauty polish

--- src/components/models/Island.vue
+++ src/components/models/Island.vue
@@ -2,6 +2,7 @@
 import { getNode, IslandScene } from '@/assets/models/Island/Island.gltf.js'
 import { addShadowAndAddToGroup } from '@/utils/threeHelper.js'
 import { shallowRef, watch } from 'vue'
+import type { Group } from 'three'
 
 const model = await getNode(IslandScene)
 const seabedPlane = await getNode(IslandScene.zligislandlvl0_seabed001)
@@ -10,7 +11,7 @@ seabedPlane.castShadow = false
 
 const groupWrapperRef = shallowRef()
 
-watch(groupWrapperRef, (newValue) => {
+watch(groupWrapperRef, (newValue: Group | undefined) => {
   if (!newValue) {
     return
   }
