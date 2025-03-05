f2a6c5cf144b880a84f715b1955dcf45f229881b
Thorsten Seyschab <business@todde.tv>
2025-03-05 22:46:27 +0100

    feat: enable fog by default

--- src/composables/useDebugStore.ts
+++ src/composables/useDebugStore.ts
@@ -5,7 +5,7 @@ export const useDebugStore = defineStore('debug', () => {
   const showVisualHelper = ref(false)
   const showLightHelper = ref(false)
   const showCameraHelper = ref(false)
-  const showFog = ref(false)
+  const showFog = ref(true)
   const showWaterWireframe = ref(false)
 
   return {
