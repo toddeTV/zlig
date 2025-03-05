52070f7bc90200729d1e215e67c0406ced2034d1
Thorsten Seyschab <business@todde.tv>
2025-03-05 11:03:30 +0100

    feat: add outline post-processing effect to selected build areas (#93)

--- src/composables/useDebugStore.ts
+++ src/composables/useDebugStore.ts
@@ -5,7 +5,7 @@ export const useDebugStore = defineStore('debug', () => {
   const showVisualHelper = ref(false)
   const showLightHelper = ref(false)
   const showCameraHelper = ref(false)
-  const showFog = ref(true)
+  const showFog = ref(false)
   const showWaterWireframe = ref(false)
 
   return {
