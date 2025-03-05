9cf7f11c6e149382a3866fa79f570bade2a5c054
Thorsten Seyschab <business@todde.tv>
2025-03-05 22:28:58 +0100

    feat: override the default ThreeJS shader to give it a fixed position
    Instead of depending on the camera position.

--- src/utils/threeHelper.ts
+++ src/utils/threeHelper.ts
@@ -1,4 +1,4 @@
-import { type Group, Object3D, type Scene } from 'three'
+import { type Group, Mesh, Object3D, type Scene, UniformsUtils, Vector3, type WebGLProgramParametersWithUniforms } from 'three'
 
 export function getLeafObjects(object: Object3D): Object3D[] {
   if (object.children.length === 0) {
@@ -33,10 +33,24 @@ export function addShadow(
   })
 }
 
+export function removeFogDependence(
+  object: Object3D,
+) {
+  object.traverse((child) => {
+    if (child instanceof Mesh) {
+      child.material.fog = false
+    }
+  })
+}
+
 export function addToGroup(
   group: Scene | Group | Object3D,
   object: Object3D,
+  objectEnableFog = false,
 ) {
+  if (objectEnableFog === false) {
+    removeFogDependence(object)
+  }
   group.add(object)
 }
 
@@ -44,7 +58,44 @@ export function addShadowAndAddToGroup(
   group: Scene | Group | Object3D,
   object: Object3D,
   shadowMode: 'both' | 'cast' | 'receive' = 'both',
+  objectEnableFog = false,
 ) {
   addShadow(object, shadowMode)
-  addToGroup(group, object)
+  addToGroup(group, object, objectEnableFog)
+}
+
+export function overrideFogShader(
+  shader: WebGLProgramParametersWithUniforms,
+  fogCenter = new Vector3(0, 0, 0),
+  fogDistanceOffset = 0,
+) {
+  shader.vertexShader = shader.vertexShader.replace(
+    `#include <fog_pars_vertex>`,
+    `#include <fog_pars_vertex>
+      #ifdef USE_FOG
+      // the center of the fog is set here, but bc it is the world center, we do not have do do anything
+      uniform vec3 fogCenter;
+      #endif
+      `,
+  )
+  shader.vertexShader = shader.vertexShader.replace(
+    `#include <project_vertex>`,
+    `
+      #ifdef USE_FOG
+      vec3 vertexWorldPosition = (modelMatrix * vec4(transformed, 1.0)).xyz;
+      vFogDepth = distance(fogCenter.xz, vertexWorldPosition.xz) + ${fogDistanceOffset.toFixed(1)};
+      #endif
+      
+      #include <project_vertex>`,
+  )
+  shader.vertexShader = shader.vertexShader.replace(
+    `#include <fog_vertex>`,
+    ``,
+  )
+
+  const uniforms = ({
+    fogCenter: { value: fogCenter },
+  })
+
+  shader.uniforms = UniformsUtils.merge([shader.uniforms, uniforms])
 }
