6a231b1cd66b4ae285f9a776470a23db5ee71964
Thorsten Seyschab <business@todde.tv>
2025-03-05 22:32:11 +0100

    chore: remove sea bed from island scene & make it a standalone model

--- src/components/models/Island.vue
+++ src/components/models/Island.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
 import { getNode, IslandScene } from '@/assets/models/Island/Island.gltf.js'
-import { addShadowAndAddToGroup } from '@/utils/threeHelper.js'
+import { addShadowAndAddToGroup, overrideFogShader, removeFogDependence } from '@/utils/threeHelper.js'
+import { Mesh, MeshStandardMaterial } from 'three'
 import { shallowRef, watch } from 'vue'
-import type { Group } from 'three'
+import type { Group, Material } from 'three'
 
 const model = await getNode(IslandScene)
-const seabedPlane = await getNode(IslandScene.zligislandlvl0_seabed001)
-seabedPlane.receiveShadow = false
-seabedPlane.castShadow = false
 
 const groupWrapperRef = shallowRef()
 
