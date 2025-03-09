98e8fe9880b33817f988455637fb0d1cf9634d14
Thorsten Seyschab <business@todde.tv>
2025-03-09 18:51:21 +0100

    chore: remove buildings `Windmill` & `CarnationShop`

--- src/game-logic/buildings/index.ts
+++ src/game-logic/buildings/index.ts
@@ -1,7 +1,2 @@
-import carnationShop from '@/game-logic/buildings/carnationShop.js'
-import windmill from '@/game-logic/buildings/windmill.js'
-
 export const buildingTypes = {
-  carnationShop,
-  windmill,
 }
