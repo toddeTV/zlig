8bb76ad8bf3eae10ef7ea554a94b3dc414351421
Thorsten Seyschab <business@todde.tv>
2025-03-10 13:12:22 +0100

    feat: add building `Market`

--- src/game-logic/buildings/index.ts
+++ src/game-logic/buildings/index.ts
@@ -1,11 +1,13 @@
 import Blacksmith from '@/game-logic/buildings/Blacksmith.js'
 import HomeA from '@/game-logic/buildings/HomeA.js'
 import Lumbermill from '@/game-logic/buildings/Lumbermill.js'
+import Market from '@/game-logic/buildings/Market.js'
 import Windmill from '@/game-logic/buildings/Windmill.js'
 
 export const buildingTypes = {
   Blacksmith,
   HomeA,
   Lumbermill,
+  Market,
   Windmill,
 }
