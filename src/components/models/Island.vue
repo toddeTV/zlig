10a3e03d445e5efa201a82bb0d06a75f20d44d61
Thorsten Seyschab <business@todde.tv>
2025-04-04 18:45:31 +0200

    feat: add flat shading, change grass color & smaller shadow bias (#160)

--- src/components/models/Island.vue
+++ src/components/models/Island.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { getNode, IslandScene } from '@/assets/models/Island/Island.gltf.js'
 import { addShadowAndAddToGroup } from '@/utils/threeHelper.js'
+import { type Group, Mesh } from 'three'
 import { shallowRef, watch } from 'vue'
-import type { Group } from 'three'
 
 const model = await getNode(IslandScene)
 
@@ -12,7 +12,24 @@ const { stop } = watch(groupWrapperRef, (newValue: Group | undefined) => {
   if (!newValue) {
     return
   }
+
+  // TODO add this in the blender model and not in the code
+  model.traverse((child) => {
+    if (child instanceof Mesh) {
+      // flat shading
+      child.material.flatShading = true
+
+      // change grass color
+      if (child.material.name === 'grass') {
+        child.material.color.r = 0.149
+        child.material.color.g = 0.847
+        child.material.color.b = 0.515
+      }
+    }
+  })
+
   addShadowAndAddToGroup(newValue, model, 'both')
+
   stop()
 })
 </script>
