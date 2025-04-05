5489bc6cf6375ff7212196a4c69ec623e2bbdd0a
Thorsten Seyschab <business@todde.tv>
2025-04-05 10:21:09 +0200

    chore: upgrade to dependency `@iconify/tailwind4`

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
-import { addDynamicIconSelectors } from '@iconify/tailwind'
-
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: false, // or 'media' or 'class'
   plugins: [
-    addDynamicIconSelectors(),
   ],
   purge: [
     './index.html',
