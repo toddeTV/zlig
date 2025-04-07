550e6a643c3bee1f5eaeef76beee4106fc5f7f58
Thorsten Seyschab <business@todde.tv>
2025-04-07 19:26:11 +0200

    feat: new base game view UI+UX with bottom user interaction bar

--- src/views/GameView.vue
+++ src/views/GameView.vue
@@ -1,69 +1,30 @@
 <script setup lang="ts">
-import GameCanvas from '@/components/ui/layout/game/GameCanvas.vue'
-import SideBar from '@/components/ui/layout/game/sidebar/SideBar.vue'
-import TopBar from '@/components/ui/layout/game/topbar/TopBar.vue'
-import { useSelectedBuildingAreaStore } from '@/composables/useSelectedBuildingAreaStore.js'
-import { ref } from 'vue'
-
-const sidebarOpen = ref(false)
-function toggleSidebar() {
-  sidebarOpen.value = !sidebarOpen.value
-}
-
-const selectedBuildingArea = useSelectedBuildingAreaStore()
-selectedBuildingArea.$subscribe((_, newId) => {
-  if (newId) {
-    // The user selected a building area.
-    sidebarOpen.value = true
-  }
-})
 </script>
 
 <template>
-  <TopBar />
-
-  <div class="relative flex flex-1 min-h-0">
-    <span
-      class="
-        md:hidden
-        absolute left-0 top-[7.5%] transition-[left]
-        shadow-sm rounded-r-full pl-1 py-2 pr-3 flex items-center text-xl z-20 bg-white/75
-      "
-      :class="{ 'left-[90%]': sidebarOpen }"
-      title="Toggle menu"
-      @click="toggleSidebar"
-    >
-      <span :class="{ 'icon-[ph--list]': !sidebarOpen, 'icon-[ph--x]': sidebarOpen }" />
-    </span>
-
-    <div
-      class="
-        absolute md:relative md:left-0 right-0 top-0 bottom-0
-        -left-[100%] transition-[left]
-        max-w-[90%] md:max-w-none md:w-[25%] lg:w-[22.5%] xl:w-[20%]
-        overflow-hidden z-30 shadow-sm
-        flex flex-col
-      "
-      :class="{ 'left-0': sidebarOpen }"
-    >
-      <SideBar />
+  <div class="relative flex flex-col w-dvw min-w-dvw max-w-dvw h-dvh min-h-dvh max-h-dvh overflow-hidden">
+    <div class="z-10 absolute flex top-0 place-self-center">
+      <TimeDisplayWithControl />
     </div>
-
-    <span
-      class="
-        absolute left-0 right-0 top-0 bottom-0 md:hidden
-        opacity-0 pointer-events-none
-        transition-opacity bg-black z-10
-      "
-      :class="{ 'opacity-50': sidebarOpen, 'pointer-events-auto': sidebarOpen }"
-      @click="toggleSidebar"
-    />
-
-    <div class="flex-1 relative">
+    <div class="z-0 relative w-full h-full">
       <GameCanvas />
+    </div>
+    <div class="z-10 absolute flex flex-row justify-between heightWithBreakpoints bottom-0 w-full bg-white">
+      <div class="relative w-full h-full">
+        <SelectedBuildingArea />
+        <div class="absolute h-[40px] -top-[40px] right-0 bg-white pl-4 pr-2 pt-2 rounded-tl-full">
+          <CurrentResources />
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+/* set the height of the bottom bar depending on the user display height (instead of width breakpoints) */
+.heightWithBreakpoints {
+  @apply [@media_((min-height:0px)_and_(max-height:500px))]:h-[100px];
+  @apply [@media_((min-height:500px)_and_(max-height:1000px))]:h-[150px];
+  @apply [@media(min-height:1000px)]:h-[200px];
+}
 </style>
