d8ba83beb16d7f620d5bb6ba0a2c94c2cff650a3
Thorsten Seyschab <business@todde.tv>
2025-09-23 16:30:29 +0200

    fix(review): add role button to div
    https://github.com/toddeTV/zlig/pull/164#discussion_r2372224555

--- src/components/ui/layout/game/sidebar/BuildingRow.vue
+++ src/components/ui/layout/game/sidebar/BuildingRow.vue
@@ -84,6 +84,7 @@ function build() {
     >
       <div
         class="flex border border-gray-300 rounded-sm bg-gray-100 h-full aspect-square overflow-hidden"
+        role="button"
       >
         <img
           :alt="`Preview image of ${props.buildingType.name}`"
